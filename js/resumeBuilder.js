6f07316f579d6579eb9136011831d19309eb508f
Basmah AlMashari <b.almashari@tamkeentech.sa>
2017-08-16 02:36:23 +0300

    objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,47 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name": "Basma",
+    "role": "Front-end developer",
+    "contacts": {
+        "mobile" : "0540335701",
+        "email" : "Basmah.almashari@gmail.com",
+        "github" : "https://github.com/Basmah-am",
+        "twitter" : "@Basmah_al",
+        "location" : "Riyadh"
+    },
+    "welcomeMessage" : "Hello to my resume",
+    "skills" : ['skill', 'skill2'],
+    "biopic" : "url"
+};
+
+var education = {
+        "schools" : [
+            {
+                "name": "",
+                "location": "string",
+                "degree": "string",
+                "majors": [],
+                "dates": "string (works with a hyphen between them)",
+                "url": "string (optional)"
+            }
+        ],
+        "onlineCourses" : [ {
+        "title": "string",
+                "school": "string",
+                "dates": "string",
+                "url": "string"  
+    },
+    { 
+        "title": "string",
+        "school": "string",
+        "dates": "string",
+        "url": "string"  
+    }
+
+    ]
+
+};
+
+
+
+
+
